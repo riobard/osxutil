9435a69dbdfa8ceb808312c47785e90d17a8e10f
Riobard <me@riobard.com>
2010-11-14 12:09:03 -0330

    Option to revert to default and fake-safari

--- disguise-safari-as-ipad.sh
+++ disguise-safari-as-ipad.sh
@@ -4,3 +4,6 @@
 # Kinda neat. 
 
 defaults write com.apple.Safari CustomUserAgent '"Mozilla/5.0 (iPad; U; CPU OS 3_2_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B500 Safari/531.21.10"'
+
+# uncomment the folloiwng line to revert to default
+# defaults delete com.apple.Safari CustomUserAgent
